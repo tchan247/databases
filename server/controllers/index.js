5fedbfc456678136373fa77934cf8a741bd081c3
tchan247 <tchan247@gmail.com>
2015-06-05 17:24:58 -0700

    Post requests add to database

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -7,20 +7,34 @@ module.exports = {
     get: function (req, res) {
       models.messages.get(function(rows){
         console.log("Rows: " + JSON.stringify(rows));
+        res.status(200);
+        res.send(JSON.stringify(rows));
       });
     }, // a function which handles a get request for all messages
-    post: function (req, res) {} // a function which handles posting a message to the database
+    post: function (req, res) {
+      // var data = JSON.parse(req.data);
+      console.log(req.data);
+      models.messages.post(req.data, function(){
+        res.send();
+      });
+    } // a function which handles posting a message to the database
   },
 
   users: {
     // Ditto as above
     get: function (req, res) {
       models.users.get(function(rows){
-
+        console.log("Rows: " + JSON.stringify(rows));
+        res.status(200);
+        res.send(JSON.stringify(rows));
       });
     },
-    post: function (req, res) {}
+    post: function (req, res) {
+      models.users.post(req, function(){
+        res.send();
+      });
+    }
   }
 };
 
-module.exports.messages.get();
+// module.exports.messages.post({data:{name: "terry", roomname:"HR", text:"hello"}});
--- server/models/index.js
+++ server/models/index.js
@@ -4,27 +4,44 @@ var db = require('../db');
 module.exports = {
   messages: {
     get: function (callback) {
-      db.connection.query("SELECT * FROM messages", function(err, rows, fields) {
+      db.connection.query("SELECT * FROM messages;", function(err, rows, fields) {
         if(err) throw err;
 
         callback(rows);
         db.connection.end();
       })
     }, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    post: function (data, callback) {
+      db.connection.query("INSERT into messages (name, roomname, text) values ('" + data.name
+        + "','" + data.roomname + "','" + data.text + "');"
+      ,function(err, rows, fields) {
+        if(err) throw err;
+
+        callback();
+        db.connection.end();
+      })
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
     get: function (callback) {
-       db.connection.query("SELECT * FROM users", function(err, rows, fields) {
+       db.connection.query("SELECT * FROM users;", function(err, rows, fields) {
          if(err) throw err;
 
          callback(rows);
          db.connection.end();
        })
     },
-    post: function () {}
+    post: function (data, callback) {
+      db.connection.query("INSERT into messages (name) values ('" + data.name + "');"
+      ,function(err, rows, fields) {
+        if(err) throw err;
+
+        callback();
+        db.connection.end();
+      })
+    }
   }
 };
 
